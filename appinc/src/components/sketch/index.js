b148d206d105063d606e707baa48ab4e2398f178
Paulo Henrique <paulohfcmendes@gmail.com>
2019-02-01 23:17:06 -0200

    fix: picker de imagens

--- appinc/src/components/sketch/index.js
+++ appinc/src/components/sketch/index.js
@@ -52,31 +52,38 @@ export default class Sketch extends Component {
               visible={showScanner}
               onRequestClose={() => {}}
             >
-              <View style={styles.container}>
-              <RNSketchCanvas
-            containerStyle={{ backgroundColor: 'transparent', height: 50 ,flex: 1 }}
-            canvasStyle={{ backgroundColor: 'transparent',  flex: 1 }}
+ <View style={styles.container}>
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          <RNSketchCanvas
+            containerStyle={{ backgroundColor: 'transparent', flex: 1 }}
+            canvasStyle={{ backgroundColor: 'transparent', flex: 1 }}
             defaultStrokeIndex={0}
             defaultStrokeWidth={5}
             changeImg={<View style={styles.functionButton}>
-            <Picker
-              selectedValue={this.state.fundo}
-              style={{height: 50, width: 100}}
-              onValueChange={(value => this.setState({ fundo: value }) )}>
+              <Picker
+                selectedValue={this.state.fundo}
+                style={{height: 50, width: 50}}
+                onValueChange={(value => this.setState({ fundo: value }) )}>
 
-              <Picker.Item label="Croqui" value= 'croqui.png' />                
-              <Picker.Item label="Vítima" value='img.jpg' />
-              <Picker.Item label="Bodybuilder" value='img2.png' />
+                <Picker.Item label="Croqui" value= 'croqui.png' />                
+                <Picker.Item label="Vítima" value='img.jpg' />
+                <Picker.Item label="Bodybuilder" value='img2.png' />
 
-            </Picker>
+              </Picker>
             </View>}
             closeComponent={<View style={styles.functionButton}><Text onPress={() => this.setState({ showScanner: false, showButton: true }) } style={{color: 'red', fontWeight: 'bold'}}>Fechar</Text></View>}
             saveComponent={<View style={styles.functionButton}><Text  style={{color: 'green', fontWeight: 'bold'}}>Salvar</Text></View>}
-
             undoComponent={<View style={styles.functionButton}><Text style={{color: 'white'}}>Desfazer</Text></View>}
             clearComponent={<View style={styles.functionButton}><Text style={{color: 'white'}}>Limpar</Text></View>}
             eraseComponent={<View style={styles.functionButton}><Text style={{color: 'white'}}>Apagar</Text></View>}
-           
+            strokeComponent={color => (
+              <View style={[{ backgroundColor: color }, styles.strokeColorButton]} />
+            )}
+            strokeSelectedComponent={(color, index, changed) => {
+              return (
+                <View style={[{ backgroundColor: color, borderWidth: 2 }, styles.strokeColorButton]} />
+              )
+            }}
             strokeWidthComponent={(w) => {
               return (<View style={styles.strokeWidthButton}>
                 <View  style={{
@@ -85,7 +92,6 @@ export default class Sketch extends Component {
                 }} />
               </View>
             )}}
-
             localSourceImage={
               {
                 filename: this.state.fundo,  
@@ -93,18 +99,18 @@ export default class Sketch extends Component {
                 mode: 'AspectFill'
               }
             }
-            
             savePreference={() => {
               return {
-                folder: 'Croqui',
+                folder: 'RNSketchCanvas',
                 filename: String(Math.ceil(Math.random() * 100000000)),
                 transparent: false,
                 imageType: 'png'
               }
-
             }}
           />
-              </View>
+        </View>
+      </View>
+             
            </Modal>
         
        </View>
